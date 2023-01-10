239f3cbcb898c3fb2bcb0683a95bbeaa6e67027c
Jhezem <mmartinhez08@gmail.com>
2023-01-09 18:03:43 -0600

    AddBook screen

---
+++ screens/AddBook.js
@@ -0,0 +1,10 @@
+import Form from "../components/Form/Form";
+import { ScrollView } from "native-base";
+
+export default function AddBook() {
+  return (
+    <ScrollView w={"100%"} h={"100%"}>
+    <Form />
+    </ScrollView>
+  )
+}
