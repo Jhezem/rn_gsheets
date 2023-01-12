9279d006093c773d42e805684a4dab852948cc43
Jhezem <mmartinhez08@gmail.com>
2023-01-11 20:35:46 -0600

    add crud functions module

---
+++ utils/crudFunctions.js
@@ -0,0 +1,28 @@
+/**
+ * Update Book information
+ *
+ * @param {function} Navigation - React Navigation
+ * @param {array} books - Books data
+ * @param {string} bookId - The id of the book to update
+ */
+export const update = (navigation, books, bookId) => {
+  const currentBook = books.find(book => book.id === bookId);
+  navigation.navigate('UpdateBook', {book: currentBook});
+};
+
+/**
+ * Remove Book from database
+ *
+ * @param {string} id - The id of the book to remove
+ * @param {function} mutate - React-Query Mutate function
+ */
+export const remove = (id, mutate) => {
+  const libroData = {
+    id: id,
+    action: 'delete',
+    collection: 'books',
+  };
+  mutate(libroData);
+};
+
+export const view = (navigation, books, bookId) => {};
