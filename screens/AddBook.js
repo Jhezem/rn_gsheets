f280a65d02103eb4fe1f8021e7a33d06b9352ecf
Jhezem <mmartinhez08@gmail.com>
2023-01-11 20:48:40 -0600

    update form component and add mutation

--- screens/AddBook.js
+++ screens/AddBook.js
@@ -1,10 +1,27 @@
-import Form from "../components/Form/Form";
-import { ScrollView } from "native-base";
+import Form from '../components/Form/Form';
+import {ScrollView} from 'native-base';
+import {getFormInputs} from '../utils/formInputs';
+import { usePostBooks } from '../hooks/useBooks';
 
 export default function AddBook() {
+  const {mutate} = usePostBooks();
+  
+  const agregar = formData => {
+    const libroData = {
+      action: 'add',
+      collection: 'books',
+      data: formData,
+    };
+    mutate(libroData);
+  };
+
   return (
-    <ScrollView w={"100%"} h={"100%"}>
-    <Form />
+    <ScrollView w={'100%'} h={'100%'}>
+      <Form
+        inputs={getFormInputs()}
+        title={'Agregar Libro'}
+        onSubmit={formData => agregar(formData)}
+      />
     </ScrollView>
-  )
+  );
 }
