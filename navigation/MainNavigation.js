5e6721489087179603cb7a753d43d90b30846ee6
Jhezem <mmartinhez08@gmail.com>
2023-01-11 20:53:14 -0600

    update main navigation
    added new screens

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -3,6 +3,8 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import Inicio from '../screens/Home';
 import Book from '../screens/Book';
+import AddBook from '../screens/AddBook';
+import UpdateBook from '../screens/UpdateBook';
 
 const Stack = createStackNavigator();
 
@@ -19,11 +21,10 @@ export default function MainNavigation() {
             fontWeight: '500',
           },
         }}>
-        <Stack.Screen name="Libros" component={Inicio} />
-        <Stack.Screen
-          name="Libro"
-          component={Book}
-        />
+        <Stack.Screen name="Libros" component={Inicio}/>
+        <Stack.Screen name="Libro" component={Book} />
+        <Stack.Screen name="AddBook" component={AddBook} options={{title: 'Agregar'}}/>
+        <Stack.Screen name="UpdateBook" component={UpdateBook} options={{title: 'Actualizar'}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
