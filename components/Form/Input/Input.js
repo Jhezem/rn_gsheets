07c05880aa6b6952d386be30bcc30423c4f05fbb
Jhezem <mmartinhez08@gmail.com>
2023-01-11 20:44:50 -0600

    add value prop to input

--- components/Form/Input/Input.js
+++ components/Form/Input/Input.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {FormControl, Input} from 'native-base';
 
-export default function MyInput({label, handleChange}) {
+export default function MyInput({label, handleChange, value}) {
   return (
     <FormControl mb={5} isRequired>
       <FormControl.Label>{label}</FormControl.Label>
-      <Input onChangeText={handleChange}/>
+      <Input onChangeText={handleChange} value={value}/>
     </FormControl>
   );
 }
