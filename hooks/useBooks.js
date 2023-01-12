afca3869f653da166d975547492bb53f2ad9fedd
Jhezem <mmartinhez08@gmail.com>
2023-01-11 20:45:39 -0600

    add query invalidation

--- hooks/useBooks.js
+++ hooks/useBooks.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import {API_BASE_PATH} from '@env';
-import { useQuery, useMutation } from 'react-query';
+import { useQuery, useMutation, useQueryClient } from 'react-query';
 
 const fetchBooks = async () => {
     const data = await axios.get(`${API_BASE_PATH}?collection=books`);
@@ -17,5 +17,8 @@ export const useFetchBooks = () => useQuery(["books"], fetchBooks, {
 });
 
 export const usePostBooks = () => {
-    return useMutation(postBooks)
+    const queryClient = useQueryClient();
+    return useMutation(postBooks, {
+        onSuccess: () => queryClient.invalidateQueries('books')
+    })
 }
