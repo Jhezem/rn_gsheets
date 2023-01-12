1fdee50e083f471c5ff5db3fb93b040cd8eebb9c
Jhezem <mmartinhez08@gmail.com>
2023-01-12 16:41:33 -0600

    add Alert component

---
+++ components/Alerts/Confirm.js
@@ -0,0 +1,51 @@
+import React, {useState, useRef} from 'react';
+import {Center, AlertDialog, Button} from 'native-base';
+
+export default function Confirm({
+  isOpen,
+  heading,
+  toggle,
+  description,
+  onCancel,
+  onConfirm,
+  cancelText,
+  confirmText,
+}) {
+  const cancelRef = useRef(null);
+  return (
+    <Center>
+      <AlertDialog
+        leastDestructiveRef={cancelRef}
+        isOpen={isOpen}
+        onClose={toggle}>
+        <AlertDialog.Content>
+          <AlertDialog.CloseButton />
+          <AlertDialog.Header>{heading}</AlertDialog.Header>
+          <AlertDialog.Body>{description}</AlertDialog.Body>
+          <AlertDialog.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="unstyled"
+                colorScheme="coolGray"
+                onPress={() => {
+                  if (onCancel && typeof onCancel === 'function') onCancel();
+                  toggle();
+                }}
+                ref={cancelRef}>
+                {cancelText}
+              </Button>
+              <Button
+                colorScheme="danger"
+                onPress={() => {
+                  if (onConfirm && typeof onConfirm === 'function') onConfirm();
+                  toggle();
+                }}>
+                {confirmText}
+              </Button>
+            </Button.Group>
+          </AlertDialog.Footer>
+        </AlertDialog.Content>
+      </AlertDialog>
+    </Center>
+  );
+}
