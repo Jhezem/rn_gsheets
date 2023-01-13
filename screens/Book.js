a5c504b859eed5f63e6d3ed3c14a5fc5f211cd9d
Jhezem <mmartinhez08@gmail.com>
2023-01-12 21:30:47 -0600

    update book view
    render book data

--- screens/Book.js
+++ screens/Book.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import {ScrollView, Text} from 'native-base';
+import React, {useEffect} from 'react';
+import {VStack, Image, Text, AspectRatio} from 'native-base';
 
-export default function Book() {
+export default function Book({navigation, route}) {
+  const {book} = route.params;
+
+  useEffect(() => {
+    navigation.setOptions({
+      title: book.name,
+    });
+  }, []);
+
   return (
-    <ScrollView w={"100%"} h={"100%"}>
-      <Text>Book details</Text>
-    </ScrollView>
+    <VStack
+      h={100}
+      flexGrow={'0.3'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      mt={5}
+      space={2}>
+        <AspectRatio size={'3/4'}>
+          <Image source={{
+            uri: book.portada
+          }}
+          alt={'Portada'}
+          />
+        </AspectRatio>
+      <Text>
+        <Text bold>{book.name}</Text> es un libro creado por el autor 
+        <Text bold> {book.authorName}</Text>
+      </Text>
+      <Text>
+        Este libro se encuentra en la categoria <Text bold>{book.tipo}</Text> y tiene un costo de
+        <Text bold> ${book.price}</Text>
+      </Text>
+    </VStack>
   );
 }
