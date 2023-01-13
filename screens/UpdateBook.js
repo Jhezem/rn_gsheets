c33c6c422593848b6210a7a96687e405193dc243
Jhezem <mmartinhez08@gmail.com>
2023-01-12 21:45:17 -0600

    navigate to home after update book

--- screens/UpdateBook.js
+++ screens/UpdateBook.js
@@ -3,10 +3,17 @@ import {ScrollView} from 'native-base';
 import {getFormInputs} from '../utils/formInputs';
 import {usePostBooks} from '../hooks/useBooks';
 import Spinner from '../components/Loading/Spinner';
+import { useEffect } from 'react';
 
-export default function UpdateBook({route}) {
-  const {mutate, isLoading: isUpading} = usePostBooks();
+export default function UpdateBook({route, navigation}) {
+  const {mutate, isLoading: isUpading, isSuccess} = usePostBooks();
   const {book} = route.params;
+
+  useEffect(() => {
+    if(isSuccess){
+      navigation.navigate('Libros');
+    }
+   }, [isSuccess]);
 
   if (isUpading) {
     return <Spinner text={'Actualizando'} color={'blue'} textColor={'blue'} />;
